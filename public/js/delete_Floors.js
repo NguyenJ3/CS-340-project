ee0ce5a36c5d47434c55ad791e70f26cdd3fc3fb
Jade Nguyen <jade.vu.nguyen@gmail.com>
2023-03-09 17:03:00 -0800

    Delete function for floors

---
+++ public/js/delete_Floors.js
@@ -0,0 +1,36 @@
+function deleteFloor(floorID){
+    // Put our data we want to send in a javascript object
+    let data = {
+        id: floorID
+    };
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-floors-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            deleteRow(floorID);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+function deleteRow(floorID){
+    let table = document.getElementById("floor-table");
+    for(let i = 0, row; row = table.rows[i];i++){
+        if(table.rows[i].getAttribute("data-value") == floorID){
+            table.deleteRow(i);
+            break;
+        }
+    }
+}
