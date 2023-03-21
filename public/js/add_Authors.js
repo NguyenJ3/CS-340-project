b22bff8784706b0d5cdccfad722a39c1a66e198b
Jade Nguyen <jade.vu.nguyen@gmail.com>
2023-03-20 20:38:51 -0700

    added input protection

--- public/js/add_Authors.js
+++ public/js/add_Authors.js
@@ -7,6 +7,12 @@ addAuthorForm.addEventListener("submit", function(e){
 
     let authorNameValue = inputAuthorName.value;
 
+    
+    if(authorNameValue === ''){ // Check if input is empty
+        alert("Please enter a valid author name.");
+        return;
+    }
+
     let data = {
         authorName: authorNameValue
     };
